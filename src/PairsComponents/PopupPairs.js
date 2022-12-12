bfedc668b4a614a519d1dee5624e67f83b238cd1
alexmcgeever <117983436+alexmcgeever@users.noreply.github.com>
2022-12-12 14:09:39 +0000

    comment change

--- src/PairsComponents/PopupPairs.js
+++ src/PairsComponents/PopupPairs.js
@@ -2,6 +2,7 @@ import './PopupPairs.css';
 function PopupPairs(props) 
 {
   return (props.trigger) ? (
+    //close button for popup
     <div className="PopupPairs">
       <div className="popup-inner">
         <button className="close-btn" onClick={() => props.setTrigger(false)} >close</button>
